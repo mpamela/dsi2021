9ec7d9f4d007c71be4720bece0fa1a58658df77e
mpamela <pamelamenezes57@gmail.com>
2021-09-17 01:15:07 +0000

    api

--- src/main/java/br/univille/pameladsi2021/api/FornecedorControllerAPI.java
+++ src/main/java/br/univille/pameladsi2021/api/FornecedorControllerAPI.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +25,29 @@ public class FornecedorControllerAPI {
 
     @GetMapping
     public ResponseEntity<List<Fornecedor>> getAll(){
-        return new ResponseEntity<List<Fornecedor>>(service.getAll(), HttpStatus.OK);
+        List<Fornecedor> listaFornecedores = service.getAll();
+        return new ResponseEntity<List<Fornecedor>>(listaFornecedores, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<Fornecedor> insert(@RequestBody Fornecedor fornecedor){
+        try{
+            fornecedor.setId(0);
+            service.save(fornecedor);
+            return new ResponseEntity<Fornecedor>(fornecedor, HttpStatus.CREATED);
+        }catch (Exception ex){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PostMapping
+    public ResponseEntity<List<Fornecedor>> insertList(@RequestBody List<Fornecedor> listaFornecedores){
+        for(Fornecedor umFornecedor: listaFornecedores){
+            umFornecedor.setId(0);
+            service.save(umFornecedor);
+        }
+        return new ResponseEntity<List<Fornecedor>>(listaFornecedores,HttpStatus.CREATED);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Fornecedor> getById(@PathVariable("id") Fornecedor fornecedor){
+        return new ResponseEntity<Fornecedor>(fornecedor,HttpStatus.OK);
     }
 }
