6725c5d056d79e026b8c0d46d66ca0710d83813e
mpamela <pamelamenezes57@gmail.com>
2021-10-22 01:00:58 +0000

    implementacao logout

--- src/main/java/br/univille/pameladsi2021/security/SecurityConfigurer.java
+++ src/main/java/br/univille/pameladsi2021/security/SecurityConfigurer.java
@@ -4,6 +4,7 @@ import org.apache.catalina.authenticator.SpnegoAuthenticator.AuthenticateAction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -15,14 +16,26 @@ import br.univille.pameladsi2021.service.impl.MyUserDetailsServiceImpl;
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter  {
     
     @Autowired
-    private MyUserDetailsServiceImpl myUserDetailService;
-
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(myUserDetailService);
+    private MyUserDetailsServiceImpl myUserDetailsService;
+     
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+        .authorizeRequests()
+        .antMatchers("/fonte_dados/**").permitAll()
+        .anyRequest().authenticated().and().formLogin();
+ 
+        httpSecurity.csrf().ignoringAntMatchers("/fonte_dados/**");
+        httpSecurity.headers().frameOptions().sameOrigin();
     }
-
+     
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService);
+    }
+     
     @Bean
-    public PasswordEncoder passawordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 }
