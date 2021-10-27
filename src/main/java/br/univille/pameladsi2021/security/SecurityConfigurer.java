1501a3d781daa71128c541e8c8485c267539bd6f
mpamela <pamelamenezes57@gmail.com>
2021-10-27 01:25:49 +0000

    jwt token

--- src/main/java/br/univille/pameladsi2021/security/SecurityConfigurer.java
+++ src/main/java/br/univille/pameladsi2021/security/SecurityConfigurer.java
@@ -3,12 +3,15 @@ package br.univille.pameladsi2021.security;
 import org.apache.catalina.authenticator.SpnegoAuthenticator.AuthenticateAction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import br.univille.pameladsi2021.service.impl.MyUserDetailsServiceImpl;
 
@@ -17,19 +20,29 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter  {
     
     @Autowired
     private MyUserDetailsServiceImpl myUserDetailsService;
+
+    @Autowired
+    private JWTRequestFilter jwtRequestFilter;
      
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
         .authorizeRequests()
-        .antMatchers("/fonte_dados/**").permitAll()
+        .antMatchers("/fonte_dados/**","/api/v1/auth/signin").permitAll()
         .anyRequest().authenticated().and().formLogin();
+        //.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        //httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
  
-        httpSecurity.csrf().ignoringAntMatchers("/fonte_dados/**");
+        httpSecurity.csrf().ignoringAntMatchers("/fonte_dados/**","/api/v1/auth/signin");
         httpSecurity.headers().frameOptions().sameOrigin();
     }
-     
     @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception{
+        return super.authenticationManager();
+    }
+
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService);
     }
